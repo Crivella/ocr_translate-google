d82edafc31c3c0e3aa95f7d3c3949cbb34a0acfc
Crivella <davidecrivella@yahoo.it>
2023-09-20 17:14:51 +0200

    Adapted for ocr_translate==0.2.1

--- ocr_translate_google/__init__.py
+++ ocr_translate_google/__init__.py
@@ -19,3 +19,15 @@
 """Plugin to implement google translation for ocr_translate."""
 
 __version__ = '0.1.0'
+
+tsl_model_data = {
+    "name": "google_translate",
+    "lang_src": ['af', 'sq', 'am', 'ar', 'hy', 'as', 'ay', 'az', 'bm', 'eu', 'be', 'bn', 'bs', 'bg', 'co', 'he', 'hr', 'cs', 'da', 'en', 'eo', 'et', 'ee', 'fi', 'fr', 'gl', 'ka', 'de', 'gn', 'gu', 'ha', 'he', 'hi', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'rw', 'ko', 'ku', 'lo', 'la', 'lv', 'ln', 'lt', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'ne', 'no', 'om', 'fa', 'pl', 'qu', 'ru', 'sm', 'sa', 'sr', 'sn', 'sd', 'sk', 'sl', 'so', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'ts', 'tr', 'tk', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zh', 'zht', 'zu'],
+    "lang_dst": ['af', 'sq', 'am', 'ar', 'hy', 'as', 'ay', 'az', 'bm', 'eu', 'be', 'bn', 'bs', 'bg', 'co', 'he', 'hr', 'cs', 'da', 'en', 'eo', 'et', 'ee', 'fi', 'fr', 'gl', 'ka', 'de', 'gn', 'gu', 'ha', 'he', 'hi', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'rw', 'ko', 'ku', 'lo', 'la', 'lv', 'ln', 'lt', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'ne', 'no', 'om', 'fa', 'pl', 'qu', 'ru', 'sm', 'sa', 'sr', 'sn', 'sd', 'sk', 'sl', 'so', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'ts', 'tr', 'tk', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zh', 'zht', 'zu'],
+    "lang_code": "iso1",
+    "entrypoint": "google_translate.online",
+    "iso1_map": {
+        "zh": "zh-cn",
+        "zht": "zh-tw",
+    }
+}
