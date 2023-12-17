25424e3e2b1750b0591cb5588ee18a1490dc8116
Crivella <davidecrivella@yahoo.it>
2023-12-17 07:30:49 +0100

    Fixed CD bug

--- ocr_translate_google/__init__.py
+++ ocr_translate_google/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugin to implement google translation for ocr_translate."""
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 tsl_model_data = {
     'name': 'google_translate',
